97c281519a0eb58e42b05a56708a66ff3ce536d4
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-08-24 21:24:22 +0530

    ---

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import {Routes,Route} from 'react-router-dom'
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
 
 const App = () => {
   return (
     <div>
-      App
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+         <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+           <Route path='/login' element={<Login/>}/>
+           <Route path='/signup' element={<SignUp/>}/>
+           <Route path='/cart' element={<Cart/>}/>
+           <Route path='/checkout' element={<Checkout/>}/>
+           <Route path='/add-product' element={<Checkout/>}/>
+
+    </Routes>
     </div>
   )
 }

1cbc6f8242bfe4bd30fe7e37676b05dcb0623ca6
Mehul Kumar Singh <mehulkumarsingh6@gmail.com>
2025-11-27 23:29:42 +0530

    User API

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -73,6 +73,13 @@ async function loginUser(req,res){
   })
 }
 
+function logoutUser(req,res){
+  res.clearCookie("token")
+  res.status(200).json({
+    message:"User logged out successfully"
+  })
+}
+
 module.exports={
   registerUser,
   loginUser

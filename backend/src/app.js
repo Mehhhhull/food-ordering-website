08feb84958b74009b1ed1b402e73d3fc8a453061
Mehul Kumar Singh <mehulkumarsingh6@gmail.com>
2025-12-24 20:20:15 +0530

    backend cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,16 @@ const express=require('express');
 const cookieParser=require('cookie-parser');
 const authRoutes=require('./routes/auth.routes');
 const foodRoutes=require('./routes/food.routes');
+const cors=require('cors');
 
 const app=express();
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 app.use(cookieParser());
 app.use(express.json());//brings data to req.body to let it read in the req.body makes it readbale
+
 
 app.get("/",(req,res)=>{
     res.send("Welcome to Video Zomato Backend");

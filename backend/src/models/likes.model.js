5de45c6cf9cdba0e8f604d36971132dd2dae9fca
Mehul Kumar Singh <mehulkumarsingh6@gmail.com>
2026-02-08 23:05:57 +0530

    Like Model

---
+++ backend/src/models/likes.model.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+
+const likeSchema=new mongoose.Schema({
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'user',
+    required:true
+  }
+},{
+  timestamps:true
+})
+
+const Like=mongoose.model("like",likeSchema);
+
+module.exports=Like;

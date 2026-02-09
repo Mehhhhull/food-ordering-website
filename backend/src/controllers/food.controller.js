4ad744f9d0902cdf9750395df6bdee3f50f5a1f3
Mehul Kumar Singh <mehulkumarsingh6@gmail.com>
2026-02-09 21:44:06 +0530

    ..

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -86,6 +86,10 @@ async function saveFood(req,res){
       food:foodId,
     })
 
+    await foodModel.findByIdAndUpdate(foodId,{
+      $inc:{saveCount:-1}
+    })
+
     return res.status(200).json({
       message:"Food item unsaved successfully."
     })
@@ -94,6 +98,10 @@ async function saveFood(req,res){
   const save =await saveModel.create({
     user:user.id,
     food:foodId,
+  })
+
+  await foodModel.findByIdAndUpdate(foodId,{
+    $inc:{saveCount:1}
   })
 
   res.status(201).json({
